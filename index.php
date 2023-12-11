0169aad82173545818f245c4574b83ea00c30c79
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-12-11 09:49:15 +0530

    pulll

--- index.php
+++ index.php
@@ -9,12 +9,11 @@ $routes = [
 ];
 
 if (array_key_exists($uri, $routes)) {
-    // Use var_dump for debugging or remove this line if not needed
-    var_dump('views/' . $routes[$uri]);
+
     
     include 'views/' . $routes[$uri];
 } else {
-    var_dump('views/' . $routes[$uri]);
+
 
     include 'views/notfound.php';
 }
